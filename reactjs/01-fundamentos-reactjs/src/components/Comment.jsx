7ac84f52e786b40a1423cd200a29829015ac70e0
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-04-26 19:48:40 -0300

    feat: Application Structure - Feedback Form

---
+++ reactjs/01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com./marcelllombm.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Marcelo Borges</strong>
+              <time title="11 de Maio as 08:13h" datetime="2025-05-11 08:80:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Delete comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p>Muito bo Devon, parabéns!!</p>
+        </div>
+        <footer>
+          <ThumbsUp />
+          Aplaudir <span>20</span>
+        </footer>
+      </div>
+    </div>
+  );
+}
--- reactjs/01-fundamentos-reactjs/src/components/Post.jsx
+++ reactjs/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from "./Comment.jsx";
 import styles from "./Post.module.css";
 
 export function Post({ author }) {
@@ -21,6 +22,7 @@ export function Post({ author }) {
           Públicado há 1h
         </time>
       </header>
+
       <div className={styles.content}>
         <p>Fala galeraa 👋 </p>
         <p>
@@ -34,6 +36,20 @@ export function Post({ author }) {
           {" "}
           <a href="#">#novoprojeto</a> <a>#nlw</a> <a>#rocketseat </a>
         </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
       </div>
     </article>
   );
