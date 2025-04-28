d159f2c4fc9e27ca37bf5e2dfe6595481f792069
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-04-27 22:59:52 -0300

    feat: React Engines - Post Properties

--- reactjs/01-fundamentos-reactjs/src/components/Post.jsx
+++ reactjs/01-fundamentos-reactjs/src/components/Post.jsx
@@ -1,39 +1,66 @@
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
 import { Avatar } from "./Avatar.jsx";
 import { Comment } from "./Comment.jsx";
+
 import styles from "./Post.module.css";
 
-export function Post(props) {
-  console.log(props.author);
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFromatted = format(
+    publishedAt,
+    "d 'de' LLLL 'ás' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
+
+  const publishedDaeRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar alt="Marcelo" src="https://github.com/marcelllombm.png" />
+          <Avatar alt="Marcelo" src={author.avatarUrl} />
 
           <div className={styles.authorInfo}>
-            <strong>{props.author.name}</strong>
-            <span>{props.author.role}</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="20 abril às 07:27h" dateTime="2025-04-20">
-          Públicado há 1h
+        <time
+          title={publishedDateFromatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDaeRelativeToNow}
+
+          {/* esse codigo e a biblioteca do javascript para data (new Intl.DateTimeFormat)
+          new Intl.DateTimeFormat("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            hour: "2-digit",
+            minute: "2-digit",
+          }).format(publishedAt)
+          */}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋 </p>
-        <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀{" "}
-        </p>
-        <p>
-          👉 <a href="#">jane.design/doctorcare </a>
-        </p>
-        <p>
-          {" "}
-          <a href="#">#novoprojeto</a> <a>#nlw</a> <a>#rocketseat </a>
-        </p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                <a href="#">{line.content}</a>
+              </p>
+            );
+          }
+        })}
       </div>
 
       <form className={styles.commentForm}>
