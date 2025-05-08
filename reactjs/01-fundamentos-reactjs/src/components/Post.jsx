775fddc639efbe0389e91bc4a2c4a8d384bf4815
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-05-08 04:42:47 -0300

    feat: React Engines - Removing Comments (Immutability)

--- reactjs/01-fundamentos-reactjs/src/components/Post.jsx
+++ reactjs/01-fundamentos-reactjs/src/components/Post.jsx
@@ -22,8 +22,13 @@ export function Post({ author, publishedAt, content }) {
     setNewCommentText(event.target.value);
   }
 
-  function deleteComment(comment) {
-    console.log(`Deletar comentário ${comment}`);
+  function deleteComment(commentToDelete) {
+    // imutabilidade - as variáveis não sofrem mutação, nós criamos um novo valor(um novo espaço na memória)
+    const commentsWithoutDeletedOne = comments.filter((comment) => {
+      return comment !== commentToDelete;
+    });
+
+    setComments(commentsWithoutDeletedOne);
   }
 
   const publishedDateFromatted = format(
