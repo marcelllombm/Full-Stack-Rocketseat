2615ecd5ecc2a313c3daaa102719f3fcc17cbc33
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-06-07 10:08:15 -0300

    feat(Structuring the base): Creating Text component with CVA

--- reactjs/02-todo/src/App.tsx
+++ reactjs/02-todo/src/App.tsx
@@ -1,3 +1,15 @@
+import { Text } from "./components/Text";
+
 export function App() {
-  return <div className="text--"> Ola mundo </div>;
+  return (
+    <div className="flex  flex-col gap-2">
+      <Text variant="body-md-bold" className="text-pink-base">
+        {" "}
+        Olá mundo!
+      </Text>
+      <Text className="text-green-base"> Olá mundo!</Text>
+      <Text variant="body-md-bold"> Olá mundo!</Text>
+      <Text> Levar o dog </Text>
+    </div>
+  );
 }
