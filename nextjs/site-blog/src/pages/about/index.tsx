fc70988ef5e41eb2e7c640901f1af616a5ea6836
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-10-14 07:30:56 -0300

    next feat:learning add router

---
+++ nextjs/site-blog/src/pages/about/index.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h2>About especial</h2>
+    </div>
+  );
+}
---
+++ nextjs/site-blog/src/pages/blog/post/[...slug].tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+export default function PostPage() {
+  const router = useRouter();
+  const segments = router.query.slug as string[];
+
+  return (
+    <div>
+      <h2>Routa : {segments?.join("/")}</h2>
+    </div>
+  );
+}
