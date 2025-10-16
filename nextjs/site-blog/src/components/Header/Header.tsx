4378a205eec7e2170c2773f156abef0882f074e9
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-10-16 01:59:40 -0300

    feat(ActiveLink):creating ActiveLink component

--- nextjs/site-blog/src/components/Header/Header.tsx
+++ nextjs/site-blog/src/components/Header/Header.tsx
@@ -1,13 +1,8 @@
 import Link from "next/link";
 import { Button } from "../ui/button";
-import { useRouter } from "next/router";
-import { cn } from "@/lib/utils";
+import { ActiveLink } from "../Active-link";
 
 export function Header() {
-  const router = useRouter();
-  const isHomePage = router.pathname === "/";
-  const isBlogPage = router.pathname.startsWith("/blog");
-
   return (
     <header
       className="fixed top-0 z-50 w-full border-b
@@ -17,25 +12,11 @@ export function Header() {
         <div className="flex h-16 items-center justify-between">
           <Link href="/">Logo</Link>
           <nav className="flex items-center gap-6">
-            <Link
-              href="/"
-              className={cn(
-                "text-sm font-medium transition-colors hover:text-blue-500",
-                isHomePage ? "text-blue-500 " : "text-muted-foreground"
-              )}
-            >
-              Início
-            </Link>
-            <Link
-              href="/blog"
-              className={cn(
-                "text-sm font-medium transition-colors hover:text-blue-500",
-                isBlogPage ? "text-blue-500" : "text-muted-foreground"
-              )}
-            >
-              Blog
-            </Link>
-            <Button variant="secondary">Começar</Button>
+            <ActiveLink href="/">Início</ActiveLink>
+            <ActiveLink href="/blog">Blog</ActiveLink>
+            <Button variant="secondary" asChild>
+              <Link href="/começa">Começar</Link>
+            </Button>
           </nav>
         </div>
       </div>
