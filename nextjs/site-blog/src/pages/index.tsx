9fb7d3cdff03572acbc6b1d29fa53fce870565ab
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-10-16 01:12:48 -0300

    feat(header): creating header component

--- nextjs/site-blog/src/pages/index.tsx
+++ nextjs/site-blog/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+import { Header } from "@/components/Header";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -14,8 +14,7 @@ const geistMono = Geist_Mono({
 export default function Home() {
   return (
     <div className="min-h-screen min-w-full bg-stone-950">
-      <h1 className="text-4xl text-green-400">Titulo</h1>
-      <Button variant="secondary">Click</Button>
+      <Header />
     </div>
   );
 }
