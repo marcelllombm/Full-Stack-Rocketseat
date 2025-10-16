26f8cba660c934aed95e9591697f8072e41cb94c
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-10-15 22:16:27 -0300

    chore: install shadcn

--- nextjs/site-blog/src/pages/index.tsx
+++ nextjs/site-blog/src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { Geist, Geist_Mono } from "next/font/google";
 
 const geistSans = Geist({
@@ -14,6 +15,7 @@ export default function Home() {
   return (
     <div className="min-h-screen min-w-full bg-stone-950">
       <h1 className="text-4xl text-green-400">Titulo</h1>
+      <Button variant="secondary">Click</Button>
     </div>
   );
 }
