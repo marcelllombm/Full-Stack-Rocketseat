e1fece3ae98e762695a46a7f5607d839d171f5e6
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-10-18 16:46:16 -0300

    style:Add background SVGs for footer and features; update CallToAction and SupportSection components to use new backgrounds

--- nextjs/site-blog/src/components/call-to-action/CallToAction.tsx
+++ nextjs/site-blog/src/components/call-to-action/CallToAction.tsx
@@ -4,8 +4,13 @@ import Link from "next/link";
 
 export const CallToAction = () => {
   return (
-    <section className="p-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
-      <div className="container ">
+    <section className=" relative p-24 bg-gradient-to-b from-cyan-950/20 to-gray-700">
+      <div
+        className="absolute inset-0 bg-[url('/background-footer.svg')] 
+      bg-cover bg-center bg-no-repeat opacity-90
+      "
+      />
+      <div className="container relative">
         <div className="flex flex-col items-center gap-6 text-center">
           <div className="p-4 bg-cyan-300 rounded-full w-fit ">
             <Store className="text-cyan-100" />
