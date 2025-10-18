2c034bbedd6dea54e014b69cb98318c1c7aea976
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-10-18 15:09:09 -0300

    feat(CallToAction): create end implement CallToAction component with button and styling

--- nextjs/site-blog/src/components/ui/button.tsx
+++ nextjs/site-blog/src/components/ui/button.tsx
@@ -13,6 +13,7 @@ const buttonVariants = cva(
         destructive: "bg-destructive text-destructive-foreground hover:bg-destructive/90",
         outline:
           "border border-gray-400 bg-gray-700 transitio-colors duration-200 hover:text-blue-200 rounded-full hover:border-blue-200",
+        primary: "bg-blue-200 text-white hover:bg-blue-300 rounded-full",
         secondary: "bg-gray-100 text-gray-800 hover:bg-blue-100",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
