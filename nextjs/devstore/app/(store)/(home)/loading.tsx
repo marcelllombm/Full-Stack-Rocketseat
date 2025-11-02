e8633743eb79799d58c45879b10e850f1ee5966b
Marcelllombm <marceloborgesdemelo@macminimarcelllombm.local>
2025-11-02 16:02:02 -0300

    feat(skeleton) add the page home

--- nextjs/devstore/app/(store)/(home)/loading.tsx
+++ nextjs/devstore/app/(store)/(home)/loading.tsx
@@ -1,3 +1,11 @@
+import { Skeleton } from "@/app/components/skeleton";
+
 export default function HomeLoading() {
-  return <div className="">carregando home</div>;
+  return (
+    <div className="grid h-full grid-cols-9 grid-rows-6 gap-6">
+      <Skeleton className="col-span-6 grid-rows-6 gap-6 h-[856px] " />
+      <Skeleton className="col-span-3 grid-rows-3 " />
+      <Skeleton className="col-span-3 grid-rows-3 " />
+    </div>
+  );
 }
